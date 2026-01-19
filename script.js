9aeafb3818830644cafe471c537ea81027978412
Himesh Nama <himeshnama2004@gmail.com>
2026-01-19 20:03:40 +0530

    final touchup

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ const changeTurn = ()=>{
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0,1,2, 5,5,0],
+        [3,4,5, 5,15,0],
+        [6,7,8, 5,25,0],
+        [0,3,6, -5,15,90],
+        [1,4,7, 5,15,90],
+        [2,5,8, 15,15,90],
+        [0,4,8, 5,15,45],
+        [2,4,6, 5,15,135],
     ]
     wins.forEach(e =>{
           if((boxtext[e[0]].innerText === boxtext[e[1]].innerText)&&(boxtext[e[1]].innerText === boxtext[e[2]].innerText)&&(boxtext[e[0]].innerText !== "")){
@@ -29,6 +29,8 @@ const checkWin = ()=>{
             gameover.play();
             isgameover = true;
             document.querySelector('.imgbox ').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector('.line').style.width = "20vw";
           }
     })
 }
@@ -47,4 +49,16 @@ Array.from(boxes).forEach(element =>{
             }
         }
     })
+})
+// Add onclick Listener to reset button 
+reset.addEventListener('click',()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element =>{
+        element.innerText = ""
+    });
+    turn = "X";
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox ').getElementsByTagName('img')[0].style.width = "0px";
+    document.querySelector('.line').style.width = "0vw";
 })
